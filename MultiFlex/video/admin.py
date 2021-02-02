75b20e04eb8c5e9752adf99bb73f1f03f6d85aa5
SeungYeon <rlatmddus2500@gmail.com>
2021-02-02 11:43:02 +0900

    폴더삭제

--- MultiFlex/video/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Video
-# Register your models here.
-
-@admin.register(Video)
-
-class VideoAdmin(admin.ModelAdmin):
-    list_display = ('title', 'video_type') # 영화 목록에 보여야 할 목록(추가예정)
-
