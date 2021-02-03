657087bda74ec83b9e2b6e6cbc24a01d28ca1739
JJY <wjdwodud006@gmail.com>
2021-02-03 10:29:08 +0900

    base.html 추가

--- MultiFlex/mysite/urls.py
+++ MultiFlex/mysite/urls.py
@@ -13,10 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from os import name
 from django.contrib import admin
 from django.urls import path, include
+from mysite.views import HomeView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('tinymce/', include('tinymce.urls')),
+    path('', HomeView.as_view, name='home'),
 ]
