5c70e2514a3a4b2a7b918f4174bb64d1b34347f2
TaeSeok <xotjr1316@gmail.com>
2021-02-04 10:07:09 +0900

    video_detail

---
+++ MultiFlex/video/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+app_name = 'video'
+
+urlpatterns = [
+    # 실험
+    path('',views.VideoLV.as_view(), name='index'),
+    # detail
+    path('<int:pk>/', views.VideoDV.as_view(), name='video_detail'),
+]
