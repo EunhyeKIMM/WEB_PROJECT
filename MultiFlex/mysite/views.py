7f7cb2afe22aca0f2d8fe71aa54724cb064becc7
JJY <wjdwodud006@gmail.com>
2021-02-04 17:03:12 +0900

    dark/light mode

--- MultiFlex/mysite/views.py
+++ MultiFlex/mysite/views.py
@@ -1,4 +1,13 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, FormView
 
 class Homeview(TemplateView):
     template_name = 'home.html'
+
+    def get_context_data(self, **kwargs):
+        theme = self.request.GET.get('theme')
+        if theme : 
+            self.request.session['theme']=theme
+        return super().get_context_data(**kwargs)
+
+class SearchFormView(FormView):
+    pass
